6765934c586b7ed92547a7336557f74abbead6e1
Madhan Kumar S <madhankumar@Madhans-MacBook-Pro.local>
2025-05-04 13:33:45 +0530

    feat: 🎸 app icon and splash screen added
    app icon, splash screeen, package compatible versions, feature scripts
    
    BREAKING CHANGE: 🧨 No

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // it should be the last plugin in the array
+    // https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started#:~:text=%2C%0A%20%20%7D%3B-,CAUTION,-react%2Dnative%2Dreanimated
+    'react-native-reanimated/plugin',
+  ],
 };
