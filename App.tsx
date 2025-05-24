a9a1699ef0f99fcc4d7f9111f4847e65dad4e4ac
Pragya Sahu <pragyasahu@Pragyas-MacBook-Pro.local>
2025-05-24 19:54:08 +0530

    feat: fixed splash screen

--- App.tsx
+++ App.tsx
@@ -5,15 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import RootNavigator from './src/navigation/RootNavigator';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
-         <View style={{ flex: 1 }}>
-          <RootNavigator />
-        </View>
+    <View style={{flex: 1}}>
+      <RootNavigator />
+    </View>
   );
 }
 export default App;
