ce831581ea182eaa9dccf4f9ea3e9ecb774f8901
Pragya Sahu <pragyasahu@Pragyas-MacBook-Pro.local>
2025-05-24 20:20:53 +0530

    feat: fixed walkthrough style

--- android/app/src/main/java/com/tracetrack/MainActivity.kt
+++ android/app/src/main/java/com/tracetrack/MainActivity.kt
@@ -10,13 +10,10 @@ import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
-  // override fun onCreate(savedInstanceState: Bundle?) {
-  //   super.onCreate(null)
-  // }
-override fun onCreate(savedInstanceState: Bundle?) {
-  SplashScreen.show(this)
-  super.onCreate(null)
-}
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
+    super.onCreate(null)
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
